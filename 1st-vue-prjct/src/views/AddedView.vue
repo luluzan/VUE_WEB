4104860cace8cf96391ea11d36be920b905adfc4
Arguzan <128905745+Arguzan@users.noreply.github.com>
2023-06-13 09:59:12 +0200

    start

---
+++ 1st-vue-prjct/src/views/AddedView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="added">
+      <h1>ESto es una nueva pagina</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .added {
+      min-height: 100vh;
+      display: flex;
+      color: #fabada;
+      align-items: center;
+    }
+  }
+  </style>
