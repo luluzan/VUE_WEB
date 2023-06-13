def0eeb7a2538b4bc2926e882456cda6956257c5
César Conte Pérez <128363248+cesarconte@users.noreply.github.com>
2023-06-13 11:27:35 +0200

    created 1st-vue-prjct/src/components/H1.vue

---
+++ 1st-vue-prjct/src/components/H1.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>Mobile App Landing Page</h1>
+    <h1 class="green">{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 3rem;
+  font-size: clamp(3rem, 2.8rem + 1vw, 4rem);
+  margin: 0;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
